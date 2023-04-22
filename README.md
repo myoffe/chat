62f966ab82c7fba0f5cf2f20871d4755328223d3
Michael Yoffe <myoffe@gmail.com>
2023-04-22 18:07:31 +0300

    use mongo for message store

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Python chat app
+
+## Running with docker
+
+### Server
+```
+cd <project dir>
+docker build . -t chat-server -f docker/server/Dockerfile
+docker run -p 5000:5000 chat-server
+```
+
+### Client
+```
+cd <project dir>
+docker build . -t chat-client -f docker/client/Dockerfile
+docker run chat-client
+```
+
+## TODOs
+### Functional
+- [ ] Exclude user's own new messages from message fetching loop
+
+### Non-functional
+- [ ] Allow dockerized client to communicate with server
+- [ ] Extract common Dockerfile functionality
+- [ ] Separate projects or just the dependencies
--- server.py
+++ server.py
@@ -1,16 +1,29 @@
+import os
 from datetime import datetime
 
 from flask import Flask, request
+from pymongo import MongoClient
 
 from common import CHAT_USER_HEADER
 
+# TODO global vars
+mongo_client = MongoClient(os.environ.get('MONGODB_URL' or 'MONGODB_URL=mongodb://localhost:9999/chat'))
+database = mongo_client['chat']
+message_collection = database['messages']
+
 app = Flask(__name__)
 
-messages = []
+
+def fetch_messages():
+    return message_collection.find({}, {'_id': False})
 
 
 def get_user():
     return request.headers.get(CHAT_USER_HEADER)
+
+
+def get_db_messages(db):
+    return db['messages']
 
 
 @app.get('/messages')
@@ -20,7 +33,14 @@ def get_messages():
     except ValueError:
         since = 0
 
+    messages = fetch_messages()
+
+    # TODO use mongo query?
     return [m for m in messages if m['timestamp'] > since]
+
+
+def persist_message(user, timestamp, message_text):
+    message_collection.insert_one({'user': user, 'timestamp': timestamp, 'message': message_text})
 
 
 @app.post('/messages')
@@ -29,10 +49,10 @@ def send_message():
     if not user:
         return {'error': f'Missing username header ({CHAT_USER_HEADER})'}, 400
 
-    message = request.json.get('message', None)
-    if not message:
+    message_text = request.json.get('message', None)
+    if not message_text:
         return {'error': 'Missing message field'}, 400
 
     timestamp = datetime.now().timestamp()
-    messages.append({'user': user, 'timestamp': timestamp, 'message': message})
+    persist_message(user, timestamp, message_text)
     return {'success': True}, 201
